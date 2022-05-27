75c7a6e8d827cb9c8a13cad9abc63fcba163007a
skrzymo <skrzymo@gmail.com>
2022-05-27 08:28:28 +0200

    Initial commit - first touch with MAUI

---
+++ ColorsMAUI/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿namespace ColorsMAUI;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+        var (r, g, b) = Settings.Load();
+        SliderR.Value = r;
+        SliderG.Value = g;
+        SliderB.Value = b;
+	}
+
+    void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        Color color = Color.FromRgb(SliderR.Value, SliderG.Value, SliderB.Value);
+
+        ColorRectangle.Fill = new SolidColorBrush(color);
+
+        LabelR.Text = Math.Round(255 * color.Red).ToString();
+        LabelG.Text = Math.Round(255 * color.Green).ToString();
+        LabelB.Text = Math.Round(255 * color.Blue).ToString();
+
+        Settings.Save(SliderR.Value, SliderG.Value, SliderB.Value);
+    }
+
+    //int count = 0;
+
+    //private void OnCounterClicked(object sender, EventArgs e)
+    //{
+    //	count++;
+
+    //	if (count == 1)
+    //		CounterBtn.Text = $"Clicked {count} time";
+    //	else
+    //		CounterBtn.Text = $"Clicked {count} times";
+
+    //	SemanticScreenReader.Announce(CounterBtn.Text);
+    //}
+}
+
+
---
+++ ColorsMAUI/Settings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace ColorsMAUI
+{
+	internal static class Settings
+	{
+		private static readonly string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "colors.xml");
+
+		private static readonly IFormatProvider formatProvider = CultureInfo.InvariantCulture;
+
+		public static void Save(double r, double g, double b)
+        {
+			XDocument xml = new XDocument(
+				new XElement("settings",
+					new XElement("r", r.ToString(formatProvider)),
+					new XElement("g", g.ToString(formatProvider)),
+					new XElement("b", b.ToString(formatProvider))
+				)
+			);
+
+			xml.Save(filePath);
+        }
+
+		public static (double r, double g, double b) Load()
+        {
+			if (!File.Exists(filePath))
+				return (0.0, 0.0, 0.0);
+
+			try
+            {
+				XDocument xml = XDocument.Load(filePath);
+				double r = double.Parse(xml.Root.Element("r").Value, formatProvider);
+				double g = double.Parse(xml.Root.Element("g").Value, formatProvider);
+				double b = double.Parse(xml.Root.Element("b").Value, formatProvider);
+
+				return (r, g, b);
+			}
+			catch
+            {
+				return (0.0, 0.0, 0.0);
+            }
+        }
+	}
+}
+
